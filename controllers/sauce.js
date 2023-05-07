69adab0f21c66ec219e50650f7f335c48f51777e
mohabens <mohbenselama@gmail.com>
2023-05-07 15:41:29 +0100

    http errors fixed

--- controllers/sauce.js
+++ controllers/sauce.js
@@ -42,7 +42,7 @@ exports.modifySauce = (req, res, next) => {
   Sauce.findOne({ _id: req.params.id })
     .then((sauce) => {
       if (sauce.userId != req.auth.userId) {
-        res.status(401).json({ message: 'Not authorized' });
+        res.status(403).json({ message: 'Forbidden' });
       } else {
         Sauce.updateOne({ _id: req.params.id }, { ...sauceObject, _id: req.params.id })
           .then(() => {
@@ -51,11 +51,11 @@ exports.modifySauce = (req, res, next) => {
             }
             return res.status(200).json({ message: 'Objet modifié!' })
           })
-          .catch(error => res.status(401).json({ error }));
+          .catch(error => res.status(500).json({ error }));
       }
     })
     .catch((error) => {
-      res.status(400).json({ error });
+      res.status(404).json({ error });
     });
 };
 
@@ -65,18 +65,18 @@ exports.deleteSauce = (req, res, next) => {
   Sauce.findOne({ _id: req.params.id })
     .then(sauce => {
       if (sauce.userId != req.auth.userId) {
-        res.status(401).json({ message: 'Not authorized' });
+        res.status(403).json({ message: 'Forbidden' });
       } else {
         const filename = sauce.imageUrl;
         fs.unlink(`images/${filename}`, () => {
           Sauce.deleteOne({ _id: req.params.id })
             .then(() => { res.status(200).json({ message: 'Objet supprimé !' }) })
-            .catch(error => res.status(401).json({ error }));
+            .catch(error => res.status(500).json({ error }));
         });
       }
     })
     .catch(error => {
-      res.status(500).json({ error });
+      res.status(404).json({ error });
     });
 };
 
@@ -123,7 +123,7 @@ exports.likeDislikeSauce = (req, res, next) => {
         throw message //401
       }
     })
-    .catch((error) => res.status(404).json({ error }));
+    .catch((error) => res.status(401).json({ error }));
 };
 
 
